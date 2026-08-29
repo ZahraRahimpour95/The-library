17dbd0a824d154b58037ee78f7336e7c5c2786eb
ZahraRahimpour95 <faezehr.95@gmail.com>
2026-08-29 23:13:30 +0330

    all questions done!

--- book/urls.py
+++ book/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from .views import book_list, add_book , edit_book
+from .views import book_list, add_book , edit_book, delete_book
 
 
 urlpatterns = [
     path('books/', book_list , name = 'book_list'),
     path('books/add/', add_book , name = 'add_book' ),
     path('books/edit/<int:book_id>/' , edit_book , name = 'edit_book'),
+    path('books/delete/<int:book_id>/' , delete_book , name = 'delete_book'),
 ]
--- book/views.py
+++ book/views.py
@@ -3,7 +3,15 @@ from book.models import *
 from .forms import BookForm 
 
 def book_list (request):
+
     query = request.GET.get('q')
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    publication_date = request.GET.get('publication_date')
+
+    books = Books.objects.all()
+
+    #search
     if query:
         books = Books.objects.filter(title__icontains = query
                                      )| Books.objects.filter(
@@ -11,8 +19,46 @@ def book_list (request):
                                      )| Books.objects.filter(
                                          author__last_name__icontains = query
                                      )
-    else:
+        
+
+    is_filtered = False
+
+    #price filter
+    if min_price :
+        books = Books.objects.filter(price__gte = min_price)
+        is_filtered = True
+
+    if max_price:
+        books = Books.objects.filter(price__lte = max_price)
+        is_filtered = True
+
+    #publication_date filter 
+    if publication_date :
+        books = Books.objects.filter(publication_date__gte = publication_date)
+        is_filtered = True
+
+    # Delete filtered books
+    if request.method == 'POST' and is_filtered :
+
+        min_price = request.POST.get('min_price')
+        max_price = request.POST.get('max_price')
+        publication_date = request.POST.get('publication_date')
+
         books = Books.objects.all()
+
+        if min_price:
+            books = books.filter(price__gte=min_price)
+
+        if max_price:
+            books = books.filter(price__lte=max_price)
+
+        if publication_date:
+            books = books.filter(publication_date__gte=publication_date)
+
+        books.delete()
+
+        return redirect('book_list')
+        
 
     return render (
         request, 
@@ -56,3 +102,9 @@ def edit_book(request, book_id):
         'book/edit_book.html',
         {'form': form}
     )
+
+def delete_book(request, book_id):
+    book = get_object_or_404(Books , id = book_id)
+
+    book.delete()
+    return redirect('book_list')
